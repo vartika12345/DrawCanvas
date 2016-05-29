01fe0a2027e300db664c21eebd7b55b19362545d
VARTIKA SHARMA <vartika.1403@gmail.com>
2016-05-30 00:08:40 +0530

    Added circle.xml file

--- app/src/main/java/com/example/user/zimmbertest/MainActivity.java
+++ app/src/main/java/com/example/user/zimmbertest/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.user.zimmbertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+       // toolbar = (Toolbar) findViewById(R.id.tbMain);
+        //setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //toolbar.setT
+
     }
 }
