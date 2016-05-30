ce2015921fa2f003c99edbbe4cc33b17cc31f7c9
VARTIKA SHARMA <vartika.1403@gmail.com>
2016-05-31 01:44:08 +0530

    Added circle to canvas

--- app/src/main/java/com/example/user/zimmbertest/CircleAdapater.java
+++ app/src/main/java/com/example/user/zimmbertest/CircleAdapater.java
@@ -1,6 +1,8 @@
 package com.example.user.zimmbertest;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,34 +24,27 @@ public class CircleAdapater extends RecyclerView.Adapter<CircleAdapater.ViewHold
     //private int[] colors;
     List<Integer> colors = new ArrayList<Integer>();
     Context context;
+    private ItemClickListener clickListener;
 
-    public CircleAdapater(List<Integer> colors,Context context)
+    public CircleAdapater(List<Integer> colors,Context context,ItemClickListener clickListener)
     {
         this.colors = colors;
         this.context = context;
+        this.clickListener = clickListener;
     }
+
+
     @Override
     public CircleAdapater.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_circle,parent,false);
-        return new ViewHolder(itemView);
+        return new ViewHolder(itemView, clickListener);
     }
 
     @Override
-    public void onBindViewHolder(CircleAdapater.ViewHolder holder, int position) {
+    public void onBindViewHolder(final CircleAdapater.ViewHolder holder, int position) {
         holder.ivCircle.setImageResource(R.drawable.circle);
-       GradientDrawable gd = (GradientDrawable)holder.ivCircle.getDrawable().getCurrent();
+        final GradientDrawable gd = (GradientDrawable)holder.ivCircle.getDrawable().getCurrent();
         gd.setColor(colors.get(position));
-        holder.setOnClickListner(new ItemClickListener() {
-            @Override
-            public void onClick(View view, int position, boolean isLongClick) {
-                Toast.makeText(context,"This is the position",Toast.LENGTH_LONG).show();
-            }
-        });
-
-       // holder.ivCircle.setBackgroundColor(colors[position]);
-      // holder.ivCircle.setBackgroundColor(getResources().getColor(R.id.your_color);
-       // holder.ivCircle
-
     }
 
     @Override
@@ -56,13 +52,19 @@ public class CircleAdapater extends RecyclerView.Adapter<CircleAdapater.ViewHold
         return colors.size();
     }
 
+
+
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
        public ImageView ivCircle;
         private ItemClickListener clickListener;
-        public ViewHolder(View itemView) {
+
+        public ViewHolder(View itemView, ItemClickListener clickListener) {
             super(itemView);
             ivCircle = (ImageView)itemView.findViewById(R.id.ivCircle);
+
             itemView.setTag(itemView);
+            this.clickListener = clickListener;
+
             itemView.setOnClickListener(this);
             itemView.setOnLongClickListener(this);
 
@@ -71,22 +73,18 @@ public class CircleAdapater extends RecyclerView.Adapter<CircleAdapater.ViewHold
 
         @Override
         public void onClick(View v) {
-            clickListener.onClick(v,getAdapterPosition(),false);
+            clickListener.onClick(colors.get(getAdapterPosition()));
 
         }
 
         @Override
         public boolean onLongClick(View v) {
-            clickListener.onClick(v,getAdapterPosition(),true);
+            clickListener.onClick( colors.get(getAdapterPosition()));
             return true;
-        }
-
-
-        public void setOnClickListner(ItemClickListener itemClickListener) {
-            this.clickListener = itemClickListener;
         }
     }
     public interface ItemClickListener {
-        void onClick(View view, int position, boolean isLongClick);
+        void onClick(int color);
     }
+
 }
--- app/src/main/java/com/example/user/zimmbertest/MainActivity.java
+++ app/src/main/java/com/example/user/zimmbertest/MainActivity.java
@@ -1,62 +1,153 @@
 package com.example.user.zimmbertest;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.user.zimmbertest.CircleAdapater.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CircleAdapater.ItemClickListener {
 
+    private static final String TAG = "ch";
     private RecyclerView rvCircle;
-    private List<Color> circle = new ArrayList<Color>();
+    private LinearLayout llCanvas;
+    public static final String MyPREFERENCES = "MyPrefs";
+
+    // private List<Color> circle = new ArrayList<Color>();
     List<Integer> colors = new ArrayList<Integer>();
     private Context context;
+    SharedPreferences sharedpreferences;
+    SharedPreferences.Editor editor;
+    private CircleAdapater.ItemClickListener itemClickListner;
+
+
+    private ImageView ivCanvasCircle;
 
     private CircleAdapater cAdapter;
-   // private int[] colors = new int[] { Color.parseColor("#F0F0F0"), Color.parseColor("#D2E4FC"),Color.parseColor() }
-
+    private Boolean flag;
+    private float x, y;
+    private int color =1;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prepareCircleItem();
         context = this;
-        cAdapter  = new CircleAdapater(colors,context);
-        rvCircle = (RecyclerView)findViewById(R.id.rvCircle);
-        LinearLayoutManager cLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        llCanvas = (LinearLayout) findViewById(R.id.llCanvas);
+        ivCanvasCircle = (ImageView) findViewById(R.id.ivCanvasCircle);
+
+        itemClickListner = this;
+        cAdapter = new CircleAdapater(colors, context, itemClickListner);
+        rvCircle = (RecyclerView) findViewById(R.id.rvCircle);
+        LinearLayoutManager cLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         rvCircle.setLayoutManager(cLayoutManager);
         rvCircle.setItemAnimator(new DefaultItemAnimator());
         rvCircle.setAdapter(cAdapter);
+        llCanvas = (LinearLayout) findViewById(R.id.llCanvas);
+        llCanvas.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    x = event.getX();
+                    y = event.getY();
+                    placeImage();
+                    flag = true;
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        editor = sharedpreferences.edit();
+        flag = sharedpreferences.getBoolean("State",false);
+        if(flag)
+        {
+            x= sharedpreferences.getFloat("X", 1);
+            y= sharedpreferences.getFloat("Y", 1);
+            color = sharedpreferences.getInt("Color", 1);
+            placeImage();
+        }
+    }
+
+    private void placeImage() {
+
+        int touchX = (int) x;
+        int touchY = (int) y;
+        ivCanvasCircle.setX(touchX);
+        ivCanvasCircle.setY(touchY);
+        ivCanvasCircle.setVisibility(View.VISIBLE);
+        ivCanvasCircle.setImageResource(R.drawable.circle);
+
+        final GradientDrawable gd = (GradientDrawable) ivCanvasCircle.getDrawable().getCurrent();
+        gd.setColor(color);
 
 
     }
 
     private void prepareCircleItem() {
         String[] colorsTxt = getApplicationContext().getResources().getStringArray(R.array.colors);
-       // List<Integer> colors = new ArrayList<Integer>();
+        // List<Integer> colors = new ArrayList<Integer>();
         for (int i = 0; i < colorsTxt.length; i++) {
             int newColor = Color.parseColor(colorsTxt[i]);
             colors.add(newColor);
         }
-      /*  circle.add(new Color(Blu);
-        circle.add();
-        circle.add(R.color.blue);
-        circle.add(R.color.brown);
-        circle.add(R.color.darkBlue);*/
+          }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
     }
 
+    @Override
+    public void onStop() {
+        Log.w(TAG, "App stopped");
+        super.onStop();
+    }
 
+    @Override
+    public void onPause() {
+        Log.w(TAG, "App Paused");
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putFloat("X", x);
+        editor.putFloat("Y", y);
+        editor.putBoolean("State",flag);
+        editor.putInt("Color",color);
+        editor.commit();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.w(TAG, "App destroyed");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(int color) {
+        this.color= color;
+        placeImage();
+    }
 }
